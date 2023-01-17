12fe3c1153fe1b241d9e2fe18f06df49db890d4a
Vann-K <114351642+Vann-K@users.noreply.github.com>
2023-01-17 17:49:39 -0500

    Commiting Early and Often.

--- js/site.js
+++ js/site.js
@@ -1,11 +1,1 @@
-function displayMessage() {
-    let msg = document.getElementById('message').value;
-
-    Swal.fire(
-        {
-            backdrop: false,
-            title: 'Yo-Ho',
-            text: msg
-        }
-    );
-}
+// Entry Function
